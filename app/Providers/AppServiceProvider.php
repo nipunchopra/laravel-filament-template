3fef18179d3fad61c462193887b59af4aee38de8
Nipun chopra <nipunchopra4@gmail.com>
2023-09-10 08:11:09 -0700

    Upgraded to filament version 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Filament\Facades\Filament;
-use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,13 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Filament::serving(function () {
-            // Using Vite
-            Filament::registerViteTheme('resources/css/filament.css');
-
-            Filament::registerScripts([
-                app(Vite::class)('resources/js/app.js'),
-            ], true);
-        });
+        //
     }
 }
